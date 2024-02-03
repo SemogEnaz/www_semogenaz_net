99d06293daccc04288fc16cacfb7c24f1a44ebec
Zane Gomes <semogenaz@gmail.com>
2024-02-04 09:35:49 +1100

    feat(details): Add mobile UI

--- src/app/weaklyPrices/pages/nav/nav.tsx
+++ src/app/weaklyPrices/pages/nav/nav.tsx
@@ -90,8 +90,8 @@ function PageCountPanel() {
     const { pageCount } = usePagerContext();
 
     const itemCountObj = makeCheckboxsObj(
-        ['', '', '', '', '', ''],
-        ['1', '2', '3', '4', '5', '6']
+        ['', '', '', '', ''],
+        ['1', '2', '3', '4', '5']
     );
 
     const itemCountComponent = makeCheckboxes(
